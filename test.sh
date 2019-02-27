5cb70ef7f9dae8885348b00a887bd995399971d4
peter-rich <yangzhanfu111@gmail.com>
2019-02-27 15:02:26 -0500

    submit

---
+++ test.sh
@@ -0,0 +1,2 @@
+python test.py --which_epoch 90 --how_many 120 --dataroot ./datasets/horse2zebra --name hosrseReal-05-05-01 --model cycle_gan --phase test --no_dropout
+
---
+++ train.sh
@@ -0,0 +1,2 @@
+python train.py --gpu_ids 3 --save_epoch_freq 10 --which_epoch 80 --which_model_netG resnet_6blocks --dataroot ./datasets/horse2giraffe --name giraffeReal-05-05-01 --model cycle_gan --no_dropout --batchSize 2 --display_id 0 --niter 20 --niter_decay 100
+
